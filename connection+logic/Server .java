86312a30f979b3910d18986da0664f46f1b68efe
fatiitava <t.fateme@gmail.com>
2017-07-14 20:09:20 +0430

    connection+logic
    but they are not working together

---
+++ connection+logic/Server .java
@@ -0,0 +1,21 @@
+
+public class Server {
+   /* public static void main(String[] args) {
+        GameManagement gameManagement=new GameManagement();
+        MapGenerator mapGenerator = new MapGenerator(gameManagement.board); //moshkel
+        mapGenerator.setSquares();
+       /* mapGenerator.setPlayers();
+        mapGenerator.setPresents();
+        mapGenerator.setScores();
+        mapGenerator.setBombs();*/
+        /*ParseS parse = new ParseS();
+        parse.parseSquares(gameManagement.board); //moshkel
+       /* parse.parsePlayers(mapGenerator.n,mapGenerator.posePlayers);
+        parse.parsePresents(mapGenerator.posPresent);
+        parse.parseScores(mapGenerator.n,mapGenerator.score);
+        parse.parseBombs(mapGenerator.bombX,mapGenerator.bombY);
+        parse.parseTime();*/
+       /* parse.parse();
+    }*/
+}
+
